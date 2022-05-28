1c5c5c3347a3cdc717f022798843356ca09a11c4
viper23 <htonnycarlos@gmail.com>
2022-05-28 12:58:16 +0200

    database creation

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import ForeignKey
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+
+    # this class help us to show new products or categories at first line after they have been added
+    class Meta:
+        ordering = ['-date_added']
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField(max_length=200)
+    category = ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    image = models.CharField(max_length=50000)
+    date_added = models.DateTimeField(auto_now=True)
+
+    # this class help us to show new products or categories at first line after they have been added
+    class Meta:
+        ordering = ['-date_added']
+
