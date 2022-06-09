deaa74c69c03dd0d37dcb6c86698a85e1620d46e
viper23 <htonnycarlos@gmail.com>
2022-06-09 08:26:34 +0200

    displaying all products on homepage

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from numpy import product
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    product_object = Product.objects.all() #selecting all products in database
+    return render(request, 'shop/index.html', {'product_object':product_object})
