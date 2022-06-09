fa198f456b7623fa7a725aea8a101bd36b570f60
viper23 <htonnycarlos@gmail.com>
2022-06-09 09:27:10 +0200

    added filter feature into searh bar

--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,9 @@ from .models import Product
 # Create your views here.
 def index(request):
     product_object = Product.objects.all() #selecting all products in database
+    item_name = request.GET.get("item-name") #retrieve the content from the search bar (on name attribute)
+    if item_name !='' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains = item_name)  
+        
+
     return render(request, 'shop/index.html', {'product_object':product_object})
