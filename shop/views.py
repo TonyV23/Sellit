1f02f7528778d1ccaf57ab092c9db189499cb959
viper23 <htonnycarlos@gmail.com>
2022-06-10 08:46:32 +0200

    pagination page linking

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from numpy import product
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     product_object = Product.objects.all() #selecting all products in database
     item_name = request.GET.get("item-name") #retrieve the content from the search bar (on name attribute)
-    if item_name !='' and item_name is not None:
+    if item_name != '' and item_name is not None :
         product_object = Product.objects.filter(title__icontains = item_name)  
-        
+    
+    # pagination 
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page) 
 
     return render(request, 'shop/index.html', {'product_object':product_object})
