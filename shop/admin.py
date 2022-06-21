349745ff6984e2b39aab8252fcc76da83c04be46
viper23 <htonnycarlos@gmail.com>
2022-06-21 22:04:24 +0200

    list display for orders in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -12,7 +12,10 @@ class AdminCategories(admin.ModelAdmin):
 class AdminProducts(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'date_added')
 
+class AdminOrders(admin.ModelAdmin):
+    list_display = ('items', 'name', 'email', 'address', 'city', 'country', 'zipcode', 'date_ordered')
+
 
 admin.site.register(Category, AdminCategories)
 admin.site.register(Product, AdminProducts)
-admin.site.register(Order)
+admin.site.register(Order, AdminOrders)
