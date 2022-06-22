c3512929d6d1e59194258d37363ca63389e48d20
viper23 <htonnycarlos@gmail.com>
2022-06-22 22:55:16 +0200

    added search_fields and list_editable features

--- shop/admin.py
+++ shop/admin.py
@@ -4,6 +4,10 @@ from .models import Category, Product, Order
 
 # Register your models here.
 
+admin.site.site_header = "La piraterie SHOP"
+admin.site.site_title = "Viper23 Shop"
+admin.site.index_title = "TonyV23"
+
 # for displaying our database tables in table
 class AdminCategories(admin.ModelAdmin):
     list_display = ('name', 'date_added')
@@ -11,6 +15,8 @@ class AdminCategories(admin.ModelAdmin):
 
 class AdminProducts(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'date_added')
+    search_fields = ('title',) # search products by it's titles
+    list_editable = ('price',)
 
 class AdminOrders(admin.ModelAdmin):
     list_display = ('items','price_to_be_paid', 'name', 'email', 'address', 'city', 'country', 'zipcode', 'date_ordered')
